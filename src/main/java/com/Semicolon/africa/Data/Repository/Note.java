ac322e6bd05a036dadf4afcc0f232d218a27f59f
oladapo444 <151373552+oladapo444@users.noreply.github.com>
2024-04-08 18:23:04 +0100

    note management system

---
+++ src/main/java/com/Semicolon/africa/Data/Repository/Note.java
@@ -0,0 +1,11 @@
+package com.Semicolon.africa.Data.Repository;
+
+import java.util.List;
+
+public interface Note {
+    List<User> Register();
+    void Search();
+    void OfflineAccess();
+    void delete();
+
+}
